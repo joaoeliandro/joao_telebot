a9bb15258047c0914132c0ab2e6d26b0c87cfec5
joaoeliandro <eliandrogermano@gmail.com>
2021-10-30 05:03:44 -0300

    Fixed port to express listen in production

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app, bot } from "./app";
 
 bot.launch();
 
-app.listen(3333, () => {
-  console.log(`Server started on port 3333`);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server started on port ${process.env.PORT || 3333}`);
 });
